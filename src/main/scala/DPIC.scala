29c7669e05f640ce15ea90bebf0e3ae4c28bfd74
Fawang <fwcheung@outlook.com>
2025-02-17 11:39:55 +0800

    chore: add prefix option

--- src/main/scala/DPIC.scala
+++ src/main/scala/DPIC.scala
@@ -20,7 +20,7 @@ import chisel3.experimental.ExtModule
 import chisel3.reflect.DataMirror
 import chisel3.util._
 import difftest._
-import difftest.batch.{BatchInfo, BatchIO}
+import difftest.batch.{BatchIO, BatchInfo}
 import difftest.common.FileControl
 import difftest.gateway.{GatewayConfig, GatewayResult, GatewaySinkControl}
 import difftest.util.Query
@@ -151,7 +151,7 @@ abstract class DPICBase(config: GatewayConfig) extends ExtModule with HasExtModu
 class DPIC[T <: DifftestBundle](gen: T, config: GatewayConfig) extends DPICBase(config) with DifftestModule[T] {
   val io = IO(Input(gen))
 
-  override def desiredName: String = gen.desiredModuleName
+  override def desiredName: String = s"${GlobalData.prefix}${gen.desiredModuleName}"
   override def modPorts: Seq[Seq[(String, Data)]] = {
     super.modPorts ++ io.elementsInSeqUInt.map { case (name, dataSeq) =>
       val prefixName = s"io_$name"
