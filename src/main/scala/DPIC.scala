fda7690c588733d98f0c7bc2cb69fb3cfafaa2e7
Fawang <fwcheung@outlook.com>
2025-02-17 11:39:55 +0800

    chore: add prefix option

--- src/main/scala/DPIC.scala
+++ src/main/scala/DPIC.scala
@@ -173,7 +173,7 @@ abstract class DPICBase(config: GatewayConfig) extends ExtModule with HasExtModu
 class DPIC[T <: DifftestBundle](gen: T, config: GatewayConfig) extends DPICBase(config) with DifftestModule[T] {
   val io = IO(Input(gen))
 
-  override def desiredName: String = gen.desiredModuleName.replace("Difftest", "DiffExt")
+  override def desiredName: String = s"${GlobalData.prefix}${gen.desiredModuleName}"
   override def modPorts: Seq[Seq[(String, Data)]] = {
     super.modPorts ++ io.elementsInSeqUInt.map { case (name, dataSeq) =>
       val prefixName = s"io_$name"
