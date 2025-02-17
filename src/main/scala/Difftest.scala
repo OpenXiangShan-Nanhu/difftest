57dafef3fff66e760d5336f724c46a82ee2bbc45
Fawang <fwcheung@outlook.com>
2025-02-17 11:39:55 +0800

    chore: add prefix option

--- src/main/scala/Difftest.scala
+++ src/main/scala/Difftest.scala
@@ -27,6 +27,10 @@ import difftest.util.Profile
 import scala.annotation.tailrec
 import scala.collection.mutable.ListBuffer
 
+object GlobalData {
+  var prefix = ""
+}
+
 trait DifftestWithCoreid {
   val coreid = UInt(8.W)
 }
