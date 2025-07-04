6c4566291117883a8570679d9649ca54bf82539e
Fawang <fwcheung@outlook.com>
2025-07-04 15:27:32 +0800

    fix: 2GHz simulation frequency

--- vcs.mk
+++ vcs.mk
@@ -64,7 +64,7 @@ VCS_FLAGS += --instr-count-dpi 1 --timing +define+VERILATOR_5
 VCS_FLAGS += -Mdir $(VCS_BUILD_DIR)  --compiler gcc
 VCS_CXXFLAGS += -std=c++20
 else
-VCS_FLAGS += -full64 +v2k -timescale=1ns/1ns -sverilog -debug_access+all +lint=TFIPC-L
+VCS_FLAGS += -full64 +v2k -timescale=100ps/10ps -sverilog -debug_access+all +lint=TFIPC-L
 VCS_FLAGS += -lca -fgp -kdb +nospecify +notimingcheck
 VCS_FLAGS += -Mdir=$(VCS_BUILD_DIR) -j200
 VCS_FLAGS += +define+VCS
