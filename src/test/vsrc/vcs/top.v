0f3bdb1fcdc0f635992dbdde80ce96bd08a88450
Chuyu Zheng <68317084+cyril0124@users.noreply.github.com>
2025-08-12 14:45:26 +0800

    feat(vcs): higher fsdb dump file limit

--- src/test/vsrc/vcs/top.v
+++ src/test/vsrc/vcs/top.v
@@ -62,7 +62,7 @@ initial begin
 `ifdef CONSIDER_FSDB
     else if (wave_type == "fsdb") begin
       $display("Dumping FSDB Waveform for DEBUG is active !!!");
-      $fsdbAutoSwitchDumpfile(10000,"tb_top.fsdb",60);
+      $fsdbAutoSwitchDumpfile(40000,"tb_top.fsdb",60);
       $fsdbDumpfile("tb_top.fsdb");
       if ($test$plusargs("mda")) $fsdbDumpMDA();
       $fsdbDumpvars(0,tb_top.sim);
